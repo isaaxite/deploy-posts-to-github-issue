55a346618ed5f0dab5c51ef17fdf77606d5eca65
isaaxite <isaacgun@outlook.com>
2023-06-13 23:03:45 +0800

    feat: add jest test-report

--- jest.config.mjs
+++ jest.config.mjs
@@ -107,6 +107,12 @@ export default {
 
   // Use this configuration option to add custom reporters to Jest
   // reporters: undefined,
+  reporters: [
+    "default",
+    ["./node_modules/jest-html-reporter", {
+      "pageTitle": "Test Report"
+    }]
+  ],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
