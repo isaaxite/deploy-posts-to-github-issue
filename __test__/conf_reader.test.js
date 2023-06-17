aac92c6737b4dc8b664f49b57c886557c5e94d4d
isaaxite <isaacgun@outlook.com>
2023-06-18 00:01:59 +0800

    feat: take a plain object to set prefix property in conf.yml

--- __test__/conf_reader.test.js
+++ __test__/conf_reader.test.js
@@ -1,5 +1,8 @@
 import { describe, test, expect } from '@jest/globals';
 import { ConfReader } from '../lib/conf_reader.js';
+import { copySync, removeSync } from 'fs-extra/esm';
+import { load as loadYaml, dump as yamlDump } from 'js-yaml';
+import { writeFileSync, readFileSync } from 'fs';
 
 describe('conf reader', () => {
   test('read conf', () => {
@@ -13,5 +16,25 @@ describe('conf reader', () => {
     expect(conf).toHaveProperty('owner');
     expect(conf).toHaveProperty('repo');
     expect(conf).toHaveProperty('token', process.env.GITHUB_TOKEN); 
-  })
+  });
+
+  test('read object prefix conf', () => {
+    const destPath = `__test__/temp/conf.${String(Date.now()).slice(2)}.yml`;
+    copySync('__test__/conf.yml', destPath);
+    const preConf = loadYaml(readFileSync(destPath));
+    preConf.prefix = {
+      owner: preConf.owner,
+      repo: preConf.repo,
+      branch: 'master',
+      dir: 'posts/'
+    };
+    writeFileSync(destPath, yamlDump(preConf));
+    // console.log(yamlDump(preConf))
+
+    const confReader = new ConfReader({ path: destPath });
+    const conf = confReader.get();
+    expect(conf.prefix).toEqual('https://raw.githubusercontent.com/isaaxite/test-repo_deploy-posts-to-github-issue/master/posts/')
+
+    removeSync(destPath);
+  });
 });
