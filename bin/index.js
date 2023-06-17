1980285197cd485a1c3df4f3533651c3ed8735a2
isaaxite <isaacgun@outlook.com>
2023-06-17 18:33:59 +0800

    feat: glue isubo to bin

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-import yargs from 'yargs'
+import yargs from 'yargs';
+import { Isubo } from '../index.js';
 
 yargs(process.argv.slice(2))
   .usage(
@@ -12,19 +13,49 @@ yargs(process.argv.slice(2))
         type: 'string'
       })
     },
-    function handler(argv) {
-      console.log(111, argv)
+    async function handler(argv) {
+      const isubo = getIsuboIns(argv);
+      await isubo.publish();
+      process.exit(0);
     }
   )
-  .command('update <posts>', 'Update only those posts that have issue_number', () => {}, (argv) => {
-    console.info(argv)
+  .command({
+    command: 'update <posts>',
+    describe: 'Update only those posts that have issue_number',
+    async handler(argv) {
+      const isubo = getIsuboIns(argv);
+      await isubo.update();
+      process.exit(0);
+    }
   })
-  .command('create <posts>', 'Mandatory creation, although the article contains issue_number', () => {}, (argv) => {
-    console.info(argv)
+  .command({
+    command: 'create <posts>',
+    describe: 'Mandatory creation, although the article contains issue_number',
+    async handler(argv) {
+      const isubo = getIsuboIns(argv);
+      await isubo.create();
+      process.exit(0);
+    }
   })
   .example([
     ['$0 publish "How to use license"', 'publish a post name "How to use license"'],
     ['$0 publish "How to use license","What is git"', 'publish several posts']
   ])
-  .demandCommand(1, 'You need at least one command before moving on')
+  .demandCommand(2, 'You need at least one command before moving on')
   .parse()
+
+function getIsuboIns(argv) {
+  const cliParams = formatArgv(argv);
+  return new Isubo({
+    cliParams,
+    confPath: 'isubo.conf.yml'
+  });
+}
+
+function formatArgv(argv) {
+  const cliParams = {
+    filename: argv.posts.split(',')
+  };
+
+  return cliParams;
+}
