70a8a0cf1b4c855127716b938ac2d18eb3d5f0cd
isaaxite <isaacgun@outlook.com>
2023-06-25 11:44:29 +0800

    feat: add dev test

--- jest.sh
+++ jest.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-node --experimental-vm-modules node_modules/jest/bin/jest.js $1 $2 $3
+node --experimental-vm-modules node_modules/jest/bin/jest.js \
+  --config=jest.dev.config.mjs \
+  $1 $2 $3
