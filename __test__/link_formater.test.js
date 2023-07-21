94886e71597eebf2e29f13e1a47a764136808e63
isaaxite <isaacgun@outlook.com>
2023-07-22 07:14:23 +0800

    chore(link_formater): add test case to complete coveragr to 100%

--- __test__/link_formater.test.js
+++ __test__/link_formater.test.js
@@ -1,7 +1,8 @@
 import { describe, test, expect } from '@jest/globals';
 import { LinkFormater } from '../lib/link_formater.js';
+import { NonEmptyStringError } from '../lib/utils/error.js';
 
-describe('link formater', () => {
+describe('Class LinkFormater', () => {
   const expextUrl = 'https://isaaxite.github.io/blog/resources/license/pic.png';
   test('add url link_prefix for relative src', () => {
     const formater1 = new LinkFormater('./license/pic.png', {
@@ -29,5 +30,43 @@ describe('link formater', () => {
       url_prefix: 'https://isaaxite.github.io/blog/resources/'
     });
     expect(formater.dest).toStrictEqual(expextUrl);
+  });
+
+
+  test(`init with empty src, it will pass, but parse to ''`, () => {
+    ['', 0, false, undefined].forEach(src => {
+      const ins = new LinkFormater(src);
+      expect(ins.dest).toEqual('');
+      expect(ins.src).toEqual('');
+    });
+  });
+
+  test('init with non-empty but non-string, it will emit err', () => {
+    [
+      1, [], {}, true, function(){}, () => {}
+    ].forEach(src => {
+      try {
+        new LinkFormater(src); 
+      } catch (error) {
+        expect(error.message).toEqual(
+          new NonEmptyStringError('src').message
+        );
+      }
+    });
+  });
+
+  test('init with conf, conf.url_prefix is non-string or empty-string.It will emit err', () => {
+    const src = './license/pic.png';
+    [
+      '', 1, [], {}, true, function(){}, () => {}
+    ].forEach(url_prefix => {
+      try {
+        new LinkFormater(src, { url_prefix })
+      } catch (error) {
+        expect(error.message).toEqual(
+          new NonEmptyStringError('url_prefix').message
+        );
+      }
+    });
   });
 });
