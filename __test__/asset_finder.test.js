7c386faef8296478ac73e7d298b40ddff12721f2
isaaxite <isaacgun@outlook.com>
2023-07-24 15:59:44 +0800

    chore(asset_finder): add test case to complete coveragr to 100%

--- __test__/asset_finder.test.js
+++ __test__/asset_finder.test.js
@@ -1,9 +1,13 @@
+import path from 'path';
 import { describe, test, expect } from '@jest/globals';
 import { existsSync } from 'fs';
-import { multiLevelSearchForResources, parse_a_md_file_and_find_nonrelative_path_asset } from './test_cases/asset_finder.js';
+import { asset_is_outside_the_source_dir, multiLevelSearchForResources, parse_a_md_file_and_find_nonrelative_path_asset } from './test_cases/asset_finder.js';
 import { AssetFinder } from '../lib/asset_finder.js';
+import { CtorParamDataObjectError, DirNotExistError, FileNotExistError, NonEmptyStringError } from '../lib/utils/error.js';
+import { getTimestampKey } from './utils/index.js';
+import { copySync, ensureDirSync, ensureFileSync, removeSync } from 'fs-extra/esm';
 
-describe('asset_finder', () => {
+describe('Class AssetFinder', () => {
   test('multi-level search for resources', () => {
     multiLevelSearchForResources(({destPath, assetPath}) => {
       // console.info({
@@ -19,31 +23,36 @@ describe('asset_finder', () => {
     });
   });
 
+  test('asset is outside the source_dir', () => {
+    const ret = asset_is_outside_the_source_dir();
+    expect(ret).toEqual('');
+  });
+
   test.each([
     {
       param: undefined,
       name: 'init with undefined will emit err',
-      getExpect: () => 'Constructor param must be object'
+      getExpect: () => new CtorParamDataObjectError().message
     },
     {
       param: {},
       name: 'init with empty object will emit err',
-      getExpect: () => 'Constructor param must be object'
+      getExpect: () => new CtorParamDataObjectError().message
     },
     {
       param: { postpath: '' },
       name: 'init with empty str for param.postpath will emit err',
-      getExpect: () => 'Must be provided non-empty string postpath'
+      getExpect: () => new NonEmptyStringError('postpath').message
     },
     {
       param: { postpath: '__test__/temp/not_exist.md' },
       name: 'init with not exist path for param.postpath will emit err',
-      getExpect: ({ param }) => `postpath: ${param.postpath} not exist!`
+      getExpect: ({ param }) => new FileNotExistError(param.postpath).message
     },
     {
       param: { postpath: '__test__/source/license.md' },
       name: 'init with not exist path for param.postpath will emit err',
-      getExpect: () => `Must be provided non-empty string assetPath`
+      getExpect: () => new NonEmptyStringError('assetPath').message
     },
     {
       param: {
@@ -51,7 +60,16 @@ describe('asset_finder', () => {
         assetPath: 'license/9720a0afdb60d23b31b3a667ad6e70a2.png'
       },
       name: 'init with not exist path for param.postpath will emit err',
-      getExpect: () => `Must be provided non-empty string sourceDirPath`,
+      getExpect: () => new NonEmptyStringError('sourceDirPath').message,
+    },
+    {
+      param: {
+        postpath: '__test__/source/license.md',
+        assetPath: 'license/9720a0afdb60d23b31b3a667ad6e70a2.png',
+        sourceDirPath: '__test__/not_exist_source/'
+      },
+      name: 'init with not-exist sourceDirPath',
+      getExpect: ({ param }) => new DirNotExistError(param.sourceDirPath).message,
     },
     {
       param: {
@@ -66,7 +84,7 @@ describe('asset_finder', () => {
       const ins = new AssetFinder(param);
       expect(ins instanceof AssetFinder).toBeTruthy();
     } catch (error) {
-      console.info(`errMsg: ${error.message}`);
+      // console.info(`errMsg: ${error.message}`);
       expect(error.message).toEqual(getExpect({ param }));
     }
   })
