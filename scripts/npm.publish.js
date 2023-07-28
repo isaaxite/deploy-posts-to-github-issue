827506e1722816faaf95e0606f59918c71fafafa
isaaxite <isaacgun@outlook.com>
2023-07-28 20:32:36 +0800

    chore: fix custom script about npm.publish

--- scripts/npm.publish.js
+++ scripts/npm.publish.js
@@ -1,11 +1,13 @@
 import fs from 'fs';
+import path from 'path';
 import prompts from 'prompts';
 import chalk from 'chalk';
 import { execSync } from 'child_process';
 
+const pkgJson= JSON.parse(fs.readFileSync('package.json', 'utf8'));
 const {
   version
-} = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+} = pkgJson;
 
 const PUBLISH_CWD = 'dist';
 
@@ -30,13 +32,64 @@ async function selectTag() {
   return tag;
 }
 
+function excludepRrops(src, ...keys) {
+  const ret = {};
+  for (const [key, val] of Object.entries(src)) {
+    if (!keys.includes(key)) {
+      ret[key] = val;
+    }
+  }
+  return ret;
+}
+
+function deleteScripts() {
+  let distPkg = pkgJson;
+  const distPkgPath = path.join(PUBLISH_CWD, 'package.json');
+  let shouldUpdate = false;
+
+  if (!fs.existsSync(distPkgPath)) {
+    throw new Error(`✖ ${chalk.redBright(`${distPkgPath} not exist!`)}`);
+  }
+
+  if (pkgJson?.scripts?.prepare) {
+    distPkg = {
+      ...pkgJson,
+      scripts: excludepRrops(
+        pkgJson.scripts,
+        'build',
+        'prepare',
+        'publish',
+        'postversion'
+      )
+    };
+
+    shouldUpdate = true;
+  }
+
+  if (shouldUpdate) {
+    fs.writeFileSync(distPkgPath, JSON.stringify(distPkg, null, 2));
+  }
+}
+
 function execPublishCmd(tag) {
   const fullCmd = `npm publish --tag ${tag}`;
   console.info(`✔ ${chalk.greenBright(`${fullCmd}\n`)}`);
+
+  deleteScripts(); 
+
   execSync(fullCmd, {
     stdio: 'inherit',
     cwd: PUBLISH_CWD
   });
+}
+
+function loginIf() {
+  const isLogin = !!execSync(`npm whoami`, {
+    encoding: 'utf8'
+  });
+  if (!isLogin) {
+    execSync('npm login', { stdio: 'inherit' });
+  }
 }
 
 async function main() {
@@ -45,10 +98,11 @@ async function main() {
       console.info(`✖ ${chalk.redBright(`${PUBLISH_CWD} missing build artifacts!`)}`);
       return;
     }
-    execSync('npm login', { stdio: 'inherit' });
+    loginIf();
     const tag = await selectTag();
     execPublishCmd(tag);
   } catch (error) {
+    console.info(`✖ ${chalk.redBright(error.message)}`);
   }
 }
 
